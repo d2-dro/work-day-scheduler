d1e101b7685a45692b47ba45905d65ce7f140ecb
duncd <du.derosier@gmail.com>
2022-07-07 23:50:11 -0400

    javascript complete for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // $("currentDay").text(moment().format('dddd, MMMM Do'));
 
+// function to determine past, present, and future events within day
+
 function timeBlockBackground() {
     var hour = moment().hours();
 
@@ -17,34 +19,39 @@ function timeBlockBackground() {
     })
 };
 
+// setting current date on document
 
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
 
 timeBlockBackground();
 
-var loadPlans = function() {
-    plans = JSON.parse(localStorage.getItem("plans"));
+// save button to add data to local storage
 
-    if (!plans) {
-        plans = {
-            plannedEvent: []
-        };
-    }
-};
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-var savePlans = function() {
-    localStorage.setItem("plans", JSON.stringify("plans"));
-};
+        localStorage.setItem(time, text);
 
+        
+    })
+})
 
+$(".saveBtn").click(function() {
+    $("article").append("<br>Appointment added to LocalStorage</br>");
+});
 
-// var savePlans = function() {
-//     localStorage.setItem("plans", JSON.stringify(plans));
-// };
+// var confirmMessage = document.getElementById("confirmation");
+// var extraMessage = document.textContent
 
-// $(.saveBtn).click(function() {
-
-//     var plans = $("#fieldEntry").val();
-//     savePlans();
-// });
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
